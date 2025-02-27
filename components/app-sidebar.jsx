95bd8717cf071eab6048af71b583e8d0d7c14b79
ang60 <angelngina98@gmail.com>
2025-02-27 15:01:33 +0300

    imagecard

--- components/app-sidebar.jsx
+++ components/app-sidebar.jsx
@@ -15,10 +15,18 @@ import {
   SidebarRail,
   SidebarFooter
 } from "@/components/ui/sidebar"
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import {
+  Settings2,
+
+} from "lucide-react"
+
 
 
 // This is sample data.
 const data = {
+  
   navMain: [
     {
       title: "Dashboard",
@@ -96,15 +104,14 @@ export function AppSidebar({
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
+             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div
-                  className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+>                  <img src="/ilab-logo.png" />
                 </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                <div className=" gap-0.5 leading-none">
+                  {/* <span className="font-semibold" >Digital Learning</span> */}
+                  {/* <span className=""></span> */}
                 </div>
               </a>
             </SidebarMenuButton>
@@ -137,9 +144,27 @@ export function AppSidebar({
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
-       <SidebarFooter>
-        {/* <NavUser user={data.user} /> */}
-      </SidebarFooter>
+       <div className="p-4">
+        <Card className="bg-purple-100 p-2 rounded-lg text-center">
+          <CardContent className="flex flex-col items-center">
+            <Image
+              src="/sidebar1.png"
+              alt="Premium Subscription"
+              width={70}
+              height={70}
+              className="mt-2"
+            />
+            <h3 className="text-lg font-semibold">Premium Subscription</h3>
+            <p className="text-sm text-gray-600">
+              Buy Premium and get access to new courses
+            </p>
+            <button className="mt-3 px-4 py-2 bg-purple-500 text-white rounded-lg text-sm">
+              More details
+            </button>
+          </CardContent>
+        </Card>
+      </div>
+       
       <SidebarRail />
     </Sidebar>
   );
